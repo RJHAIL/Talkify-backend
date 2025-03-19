14f0993a815233bc6103dad01e86754795c334b8
Raghav_Joshi <107183430+RJHAIL@users.noreply.github.com>
2025-03-19 15:38:57 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://talkify-backend-k30b.onrender.com" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://talkify-backend-k30b.onrender.com/api" : "/api",
   withCredentials: true,
 });
